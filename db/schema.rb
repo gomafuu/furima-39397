d756fbd4ef81aa63e4f5d702b6ca02c596525722
gomafuu <a.akane.0207.trb@gmail.com>
2023-10-26 12:14:57 +0900

    エラー修正

--- db/schema.rb
+++ db/schema.rb
@@ -42,9 +42,10 @@ ActiveRecord::Schema.define(version: 2023_10_22_092518) do
     t.integer "area_id", null: false
     t.integer "deliverydate_id", null: false
     t.integer "price", null: false
-    t.integer "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -86,6 +87,7 @@ ActiveRecord::Schema.define(version: 2023_10_22_092518) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
 end
