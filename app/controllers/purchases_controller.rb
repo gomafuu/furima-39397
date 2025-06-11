8d7c16b52ec221d3ee110ec1b9f74768eb1e5a45
gomafuu <137429312+gomafuu@users.noreply.github.com>
2025-06-12 01:18:49 +0900

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :set_item, only:[:index, :create]
-  before_action :purchase_item
+  before_action :purchase_item, only: [:index, :create]
 
 
   def index
