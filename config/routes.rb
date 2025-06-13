acb499f20ee70f6226d921aa10ec12005b686c30
gomafuu <137429312+gomafuu@users.noreply.github.com>
2025-06-13 16:14:03 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
+  get 'admin/delete_items', to: 'admin#delete_items'
 
   resources :items do
     resources :purchases
